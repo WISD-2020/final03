7f70a23bfa08e62fa79c92a482b0bf1815e062d8
3a732037 <3a732037@gm.student.ncut.edu.tw>
2021-01-12 02:53:11 +0800

    後台修改

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -2,18 +2,13 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">管理後台</a>
+
+        <a class="navbar-brand" href="{{ route('admin.dashboard.index') }}">回到主控台</a>
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> 管理員 <b class="caret"></b></a>
+            <a class="navbar-brand" href="{{ route('admin.menu.index') }}">回到餐點管理</a>
             <ul class="dropdown-menu">
                 <li>
                     <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -35,7 +30,7 @@
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li class="active">
-                <a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-fw fa-dashboard"></i> 主控台</a>
+                <a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-fw fa-dashboard"></i>主控台</a>
             </li>
             <li>
                 <a href="{{ route('admin.menu.index') }}"><i class="fa fa-fw fa-edit"></i>菜單管理</a>
