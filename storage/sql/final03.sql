70304f1068313ce47930a259efb53e3419869f44
3a732037 <3a732037@gm.student.ncut.edu.tw>
2021-01-13 01:11:23 +0800

    更新資料庫

--- storage/sql/final03.sql
+++ storage/sql/final03.sql
@@ -56,6 +56,12 @@ CREATE TABLE `meals` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+INSERT INTO `meals` (`id`, `name`, `img`, `price`, `created_at`, `updated_at`) VALUES
+(1,	'起司漢堡',	'img/mainmeal/1610460178.jpg',	40,	'2021-01-12 06:02:58',	'2021-01-12 06:02:58'),
+(2,	'雙層起司漢堡',	'img/mainmeal/1610460206.png',	55,	'2021-01-12 06:03:26',	'2021-01-12 06:03:26'),
+(3,	'香魚堡',	'img/mainmeal/1610460220.jpg',	50,	'2021-01-12 06:03:40',	'2021-01-12 06:03:40'),
+(4,	'香脆薯條',	'img/mainmeal/1610460241.jpg',	30,	'2021-01-12 06:04:01',	'2021-01-12 06:04:01'),
+(5,	'潛艇堡',	'img/mainmeal/1610460265.jpg',	50,	'2021-01-12 06:04:25',	'2021-01-12 06:04:25');
 
 DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE `migrations` (
@@ -120,6 +126,9 @@ CREATE TABLE `sessions` (
   KEY `sessions_last_activity_index` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('5ewuN6xBhIx8Eeee3Q0vX712Lf7kGPkzi3UU5eCd',	1,	'::1',	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',	'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiSzJycFFvd1hJcG1EN2hWSTlPaEdHR2lQMWtVQk9QeDBjRFppdFc2YyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjY6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9tZW51Ijt9czozOiJ1cmwiO2E6MDp7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czoxNzoicGFzc3dvcmRfaGFzaF93ZWIiO3M6NjA6IiQyeSQxMCR2OGhLdERnbGVDYk4zS0FoWG1ZdDEudzl2WGx5bk9qQ2RoQUVEUnpobHhSb2U1eTRtOGhSTyI7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkdjhoS3REZ2xlQ2JOM0tBaFhtWXQxLnc5dlhseW5PakNkaEFFRFJ6aGx4Um9lNXk0bThoUk8iO30=',	1610460265),
+('ZblSXAgDJCwUP9hgdvj8r6J6YyMnE5aA73lvrZTm',	1,	'::1',	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',	'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiZlJIRUtNclpaQ29nM21hVEl1Mm92Rmk5Y3V5WnhybXVFbXJXNFNjbSI7czozOiJ1cmwiO2E6MDp7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjIxOiJodHRwOi8vbG9jYWxob3N0OjgwMDAiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkdjhoS3REZ2xlQ2JOM0tBaFhtWXQxLnc5dlhseW5PakNkaEFFRFJ6aGx4Um9lNXk0bThoUk8iO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHY4aEt0RGdsZUNiTjNLQWhYbVl0MS53OXZYbHluT2pDZGhBRURSemhseFJvZTV5NG04aFJPIjt9',	1610395452);
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -139,5 +148,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
+(1,	'111',	'111@111',	NULL,	'$2y$10$v8hKtDgleCbN3KAhXmYt1.w9vXlynOjCdhAEDRzhlxRoe5y4m8hRO',	NULL,	NULL,	NULL,	NULL,	NULL,	'2021-01-11 03:03:51',	'2021-01-11 03:03:51');
 
--- 2021-01-09 14:59:33
+-- 2021-01-12 17:07:53
